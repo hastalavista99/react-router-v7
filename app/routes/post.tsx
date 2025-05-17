8cff51b380a2cb170191bcb03b1cfe054eb586fd
hastalvista99 <githumuj@gmail.com>
2025-05-17 08:40:34 +0300

    loader and action functions

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,15 +1,46 @@
+import { Form, redirect, useFetcher } from "react-router";
 import type { Route } from "./+types/post";
 
-export async function loader({ params }: Route.LoaderArgs) {
+export async function clientLoader({ params }: Route.LoaderArgs) {
     const { postId } = params;
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-    const post = await res.json();
-    return {postId};
+    return await res.json();;
 }
 
-const Post = ({loaderData} :Route.ComponentProps) => {
+export async function clientAction({ params }: Route.ClientActionArgs) {
+    try {
+        const { postId } = params;
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+            method: "DELETE",
+        });
+
+        return { isDeleted: true };
+    } catch (error) {
+        console.error("Error deleting post:", error);
+        return { isDeleted: false };
+    }
+
+
+}
+
+const Post = ({ loaderData }: Route.ComponentProps) => {
+    const fetcher = useFetcher();
+
+    const isDeleted = fetcher.data?.isDeleted;
     return (
-        <div>Post id: {loaderData.postId}</div>
+        <div>
+            {" "}
+            {!isDeleted && (
+                <>
+                    <p>Title: {loaderData.title}</p>
+                    <p>Body: {loaderData.body}</p>
+                </>
+            )}
+
+            <fetcher.Form method="delete">
+                <button type="submit">Delete</button>
+            </fetcher.Form>
+        </div>
     )
 }
 
